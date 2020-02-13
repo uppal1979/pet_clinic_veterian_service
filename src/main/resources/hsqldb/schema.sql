1b102858e738f8e232b4cc451c1499c7f3736052
rajivuppal <rajiv.uppal@wipro.com>
2020-02-13 14:08:26 +0530

    updated for concourse pipeline

--- src/main/resources/hsqldb/schema.sql
+++ src/main/resources/hsqldb/schema.sql
@@ -1,18 +1,18 @@
 
-CREATE TABLE  vets (
+CREATE TABLE IF NOT EXISTS  vets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 INDEX(last_name)
 );
 
-CREATE TABLE  specialties (
+CREATE TABLE IF NOT EXISTS specialties (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(80),
 INDEX(name)
 );
 
-CREATE TABLE  vet_specialties (
+CREATE TABLE IF NOT EXISTS vet_specialties (
   vet_id INT(4) UNSIGNED NOT NULL,
 specialty_id INT(4) UNSIGNED NOT NULL,
 FOREIGN KEY (vet_id) REFERENCES vets(id),
