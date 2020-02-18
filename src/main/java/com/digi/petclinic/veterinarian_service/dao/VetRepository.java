a76792f0dd25e25f70016dd4d6f955ce8ea3a7d0
rajiv uppal <rajiv.uppal@wipro.com>
2020-02-18 16:38:18 +0530

    config server bind

--- src/main/java/com/digi/petclinic/veterinarian_service/dao/VetRepository.java
+++ src/main/java/com/digi/petclinic/veterinarian_service/dao/VetRepository.java
@@ -1,6 +1,7 @@
 package com.digi.petclinic.veterinarian_service.dao;
 
 import com.digi.petclinic.veterinarian_service.entity.Vet;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
