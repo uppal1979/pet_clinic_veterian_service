75412b083f77d4de21b0484d4145bdcfdbebbcfd
rajiv uppal <rajiv.uppal@wipro.com>
2020-02-19 11:42:18 +0530

    config server bind

--- src/main/resources/hsqldb/schema.sql
+++ src/main/resources/hsqldb/schema.sql
@@ -1,21 +1,20 @@
-
-CREATE TABLE IF NOT EXISTS  vets (
+CREATE TABLE  vets (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-INDEX(last_name)
-);
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  INDEX(last_name)
+) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS specialties (
+CREATE TABLE  specialties (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-name VARCHAR(80),
-INDEX(name)
-);
+  name VARCHAR(80),
+  INDEX(name)
+) engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS vet_specialties (
+CREATE TABLE  vet_specialties (
   vet_id INT(4) UNSIGNED NOT NULL,
-specialty_id INT(4) UNSIGNED NOT NULL,
-FOREIGN KEY (vet_id) REFERENCES vets(id),
-FOREIGN KEY (specialty_id) REFERENCES specialties(id),
-UNIQUE (vet_id,specialty_id)
-);
+  specialty_id INT(4) UNSIGNED NOT NULL,
+  FOREIGN KEY (vet_id) REFERENCES vets(id),
+  FOREIGN KEY (specialty_id) REFERENCES specialties(id),
+  UNIQUE (vet_id,specialty_id)
+) engine=InnoDB;
